c6db890cb3453a8680907c4a7921a8cfe247b544
Olivarès Georges <olivares.georges@gmail.com>
2013-02-20 20:08:34 +0100

    first question : provider tools

---
+++ 2A/Cryptographie/TD-init/src/td/exo2/ProviderTools.java
@@ -0,0 +1,71 @@
+package td.exo2;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.Security;
+
+/**
+ *
+ * @author Olivarès Georges <dev@olivares-georges.fr>
+ */
+public class ProviderTools {
+
+    /**
+     * Get list of Providers
+     * @return Provider[]
+     */
+    public static Provider[] getProviders() {
+        return Security.getProviders();
+    }
+
+    /**
+     * Print into StrintStream the list of Provider and their Services
+     * @param out 
+     */
+    private static void printProviders(PrintStream out) {
+        StringBuilder tmp = new StringBuilder();
+        // Print Providers
+        for (Provider p : ProviderTools.getProviders()) {
+            tmp
+                    .append(p.getName()).append(" v")
+                    .append(p.getVersion()).append(" ")
+                    .append(p.getInfo()).append(" : ")
+                    .append("\n")
+                    .append("\twith services : ");
+
+            // Print Services
+            for (Service s : p.getServices()) {
+                tmp
+                        .append("\n\t\t")
+                        .append(s.getType() + " (" + s.getAlgorithm() + ")");
+            }
+            tmp.append("\n");
+        }
+        out.println(tmp.toString());
+    }
+
+    public static int installProvider(String providerName, String providerInfo, double version) {
+        Provider provider = new Provider(providerName, version, providerInfo) {
+        };
+        return Security.addProvider(provider);
+    }
+
+    public static void installService(String providerName, String type, String serviceName, String className) {
+        Security.getProvider(providerName).put(type + "." + serviceName, className);
+    }
+
+    public static void main(String[] args) {
+        try {
+            ProviderTools.printProviders(System.out);
+            ProviderTools.installProvider("PG", "BBS", 1.0);
+            ProviderTools.installService("PG", "SecureRandom", "BBSPRNG", "td2.BBSSecureRander");
+            ProviderTools.printProviders(System.out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

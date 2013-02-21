4718afe712d631b295c4f57aa7b84b0397be76de
Olivarès Georges <olivares.georges@gmail.com>
2013-02-21 01:55:48 +0100

    add comments

--- 2A/Cryptographie/TD-init/src/td/exo2/ProviderTools.java
+++ 2A/Cryptographie/TD-init/src/td/exo2/ProviderTools.java
@@ -1,8 +1,6 @@
 package td.exo2;
 
-import java.io.OutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 
 import java.security.Provider;
 import java.security.Provider.Service;
@@ -16,6 +14,7 @@ public class ProviderTools {
 
     /**
      * Get list of Providers
+     *
      * @return Provider[]
      */
     public static Provider[] getProviders() {
@@ -24,10 +23,12 @@ public class ProviderTools {
 
     /**
      * Print into StrintStream the list of Provider and their Services
-     * @param out 
+     *
+     * @param out
      */
     private static void printProviders(PrintStream out) {
         StringBuilder tmp = new StringBuilder();
+
         // Print Providers
         for (Provider p : ProviderTools.getProviders()) {
             tmp
@@ -40,22 +41,42 @@ public class ProviderTools {
             // Print Services
             for (Service s : p.getServices()) {
                 tmp
-                        .append("\n\t\t")
-                        .append(s.getType() + " (" + s.getAlgorithm() + ")");
+                        .append("\n\t\t").append(s.getType()).append(" (")
+                        .append(s.getAlgorithm())
+                        .append(")");
             }
             tmp.append("\n");
         }
         out.println(tmp.toString());
     }
 
+    /**
+     * Install Provider
+     *
+     * @param providerName
+     * @param providerInfo
+     * @param version
+     * @return
+     */
     public static int installProvider(String providerName, String providerInfo, double version) {
+        // Provider is an abstract class
+        // So ... we make an Anonymous Class
         Provider provider = new Provider(providerName, version, providerInfo) {
         };
         return Security.addProvider(provider);
     }
 
-    public static void installService(String providerName, String type, String serviceName, String className) {
-        Security.getProvider(providerName).put(type + "." + serviceName, className);
+    /**
+     * Instann Service into Provider
+     *
+     * @param providerName
+     * @param type
+     * @param serviceName
+     * @param className
+     */
+    public static Object installService(String providerName, String type, String serviceName, String className) {
+
+        return Security.getProvider(providerName).put(type + "." + serviceName, className);
     }
 
     public static void main(String[] args) {
